58ad5819ec560f90ea8c2cb0118b4cbaab2861ba
tosh <duretsimon73@gmail.com>
2014-05-27 11:53:49 +0200

    Increase VERSION + changes in POD

--- lib/Protoview/VARS.pm
+++ lib/Protoview/VARS.pm
@@ -5,7 +5,7 @@ our @EXPORT = qw(PACKAGE VERSION AUTHOR MAIL LICENSE);
 
 
 our $PACKAGE = 'Protoview';
-our $VERSION = '1.0';
+our $VERSION = '1.0.1';
 our $AUTHOR  = 'Tosh';
 our $MAIL    = 'duretsimon73 -at- gmail -dot- com';
 our $LICENSE = 'GPLv3';
