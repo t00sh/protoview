28d67a9f984bbaa65489d714cbbf9ecf54897d80
tosh <tosh@t0x0sh.org>
2014-06-18 18:32:42 +0200

    Changes: increase version number

--- lib/Protoview/VARS.pm
+++ lib/Protoview/VARS.pm
@@ -5,7 +5,7 @@ our @EXPORT = qw(PACKAGE VERSION AUTHOR MAIL LICENSE);
 
 
 our $PACKAGE = 'Protoview';
-our $VERSION = '1.0.1';
+our $VERSION = '1.1';
 our $AUTHOR  = 'Tosh';
 our $MAIL    = 'tosh -at- t0x0sh -dot- org';
 our $LICENSE = 'GPLv3';
