5f9fc28b38e123662df4f9b4f1c69eb349bb9ee7
tosh <tosh@t0x0sh.org>
2014-06-18 18:31:13 +0200

    Changes: my email has changed

--- lib/Protoview/VARS.pm
+++ lib/Protoview/VARS.pm
@@ -7,5 +7,5 @@ our @EXPORT = qw(PACKAGE VERSION AUTHOR MAIL LICENSE);
 our $PACKAGE = 'Protoview';
 our $VERSION = '1.0.1';
 our $AUTHOR  = 'Tosh';
-our $MAIL    = 'duretsimon73 -at- gmail -dot- com';
+our $MAIL    = 'tosh -at- t0x0sh -dot- org';
 our $LICENSE = 'GPLv3';
